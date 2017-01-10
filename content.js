b16b33658c3166ea7acc72cbe144a3784bee6b17
Sean LaPlante <laplante.sean@gmail.com>
2017-01-09 20:02:57 -0500

    Using chrome storage

--- content.js
+++ content.js
@@ -1,4 +1,28 @@
 
+function TimesheetDb() {
+    this.month = 0;
+    this.month_data = null;
+
+    this.load = function(date) {
+        var ret = null;
+
+        chrome.storage.sync.get(date.toString(), function(item) {
+            console.log("Got item: ", item);
+            ret = item;
+        });
+
+        return item;
+    }
+
+    this.load_month = function(month) {
+        
+    }
+
+    this.update = function(date_map_timesheet) {
+        chrome.storage.sync.set(date_map_timesheet);
+    }
+}
+
 function parseTimesheet(appBody) {
     var d = new Date();
     var year = d.getFullYear();
@@ -19,7 +43,7 @@ function parseTimesheet(appBody) {
             if (dateStr.length >= 3) {
                 var datePrts = dateStr.split("/");
                 if (datePrts.length == 2) {
-                    // Subtract one from month b/c index @ 0
+                    // Subtract one from month b/c indexed at 0
                     var date = new Date(year, (parseInt(datePrts[0]) - 1), parseInt(datePrts[1]));
                     timesheet[i].hours.push({date:date, value:0});
                 } else {
@@ -34,19 +58,40 @@ function parseTimesheet(appBody) {
         for (var j = 0; j < timesheet[i].hours.length; j++) {
             var id = "#hrs" + i + "_" + j;
             var hours = appBody.find(id).text();
-            hours = parseInt(hours);
+            hours = parseFloat(hours);
             if (isNaN(hours)) {
                 hours = 0;
             }
             timesheet[i].hours[j].value = hours;
         }
     }
-    return timesheet;
+
+    console.log("Converting so date is key...");
+    date_map_timesheet = {};
+    for (var i = 0; i < timesheet.length; i++) {
+        var charge_str = timesheet[i].title;
+        for (var j = 0; j < timesheet[i].hours.length; j++) {
+            var key = timesheet[i].hours[j].date.toString();
+
+            if (!(key in date_map_timesheet)) {
+                date_map_timesheet[key] = [];
+            }
+
+            date_map_timesheet[key].push({
+                title: charge_str,
+                value: timesheet[i].hours[j].value
+            });
+        }
+    }
+
+    return date_map_timesheet;
 }
 
 $(document).ready(function() {
     console.log("Deltrek Activated. On Deltek page.");
     
+    var db = new TimesheetDb();
+
     $("#unitFrame").on("load", function(){
         console.log("Iframe loaded. Scanning for timesheet application...");
         appBody = $("#unitFrame").contents().find("body");
@@ -55,6 +100,7 @@ $(document).ready(function() {
             console.log("Timesheet found!");
             ts = parseTimesheet(appBody);
             console.log("Parsed: ", ts);
+            db.update(ts);
         } else {
             console.log("Not on timesheet page. App title: ", appBody.find("#appTitle").text());
         }
